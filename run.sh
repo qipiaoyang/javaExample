b1ad2d1f2089006e4ffae389bd32664a4b2348dd
齐飘洋 <qipiaoyang@outlook.com>
2020-05-19 17:50:16 +0800

    更新sh脚本

---
+++ run.sh
@@ -0,0 +1,3 @@
+#javac -d class Main.java | java -classpath ./class Main
+#javac -d class ObjectClass.java | java -classpath ./class ObjectClass
+javac -d class BaseDataType.java | java -classpath ./class BaseDataType
