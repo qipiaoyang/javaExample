3219d0365b53ebd1ec0c06f90336214e74ed87f5
齐飘洋 <qipiaoyang@outlook.com>
2020-05-19 23:16:30 +0800

    更新string类

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #javac -d class Main.java | java -classpath ./class Main
 #javac -d class ObjectClass.java | java -classpath ./class ObjectClass
-javac -d class BaseDataType.java | java -classpath ./class BaseDataType
+#javac -d class BaseDataType.java | java -classpath ./class BaseDataType
+javac -d class StringClass.java | java -classpath ./class StringClass
